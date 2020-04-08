0edb425625c71d8bdffda8b9bcaf7a6566024281
Stephen Schrauger <stephen@schrauger.com>
2020-04-08 10:23:22 -0400

    angle

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -689,10 +689,8 @@ class ucf_people_directory_shortcode {
 		if ($collapsed) {
 			$collapse_class = "collapse";
 			$expanded = "false";
-			$angle_class = "fa-angle-down";
 		} else {
 			$collapse_class = "collapse show";
-			$angle_class = "fa-angle-up";
 			$expanded = "true";
 		}
 
@@ -700,7 +698,7 @@ class ucf_people_directory_shortcode {
 <li class='menu-item-collapse' id='heading-{$top_level_term->slug}'>
     <a data-toggle='collapse' href='#collapse-{$top_level_term->slug}' aria-expanded='{$expanded}' aria-controls='collapse-{$top_level_term->slug}'>
         {$top_level_term->name}
-        <i class='fa {$angle_class}'></i>
+        <i class='fa fa-angle-down'></i>
     </a>
     
 	<div id='collapse-{$top_level_term->slug}' class='{$collapse_class}' role='tabpanel' aria-labelledby='heading-{$top_level_term->slug}' data-parent='#{$shortcode_attributes->directory_id}'>
