fbf59a62a79d4f77b554fdb048589e4023ec18f6
Stephen Schrauger <stephen@schrauger.com>
2020-04-10 14:54:04 -0400

    lint

--- ucf-people-directory.php
+++ ucf-people-directory.php
@@ -8,7 +8,7 @@ Plugin URI: https://github.com/schrauger/UCF-People-Directory
 Github Plugin URI: schrauger/UCF-People-Directory
 */
 if ( ! defined( 'WPINC' ) ) {
-    die;
+	die;
 }
 
 // add a rewrite rule for the directory page, to make urls more pretty
@@ -24,94 +24,93 @@ include plugin_dir_path( __FILE__ ) . 'includes/shortcode.php';
 
 
 class ucf_people_directory {
-    static $directory_path = 'directory';
+	static $directory_path = 'directory';
 
-    function __construct() {
-        // plugin css/js
-        add_action('enqueue_block_assets', array($this, 'add_css'));
-        add_action('enqueue_block_assets', array($this, 'add_js'));
+	function __construct() {
+		// plugin css/js
+		add_action( 'enqueue_block_assets', array( $this, 'add_css' ) );
+		add_action( 'enqueue_block_assets', array( $this, 'add_js' ) );
 
-        // plugin activation hooks
-        register_activation_hook( __FILE__, array($this,'activation'));
-        register_deactivation_hook( __FILE__, array($this,'deactivation'));
-        register_uninstall_hook( __FILE__, array($this,'deactivation'));
-    }
+		// plugin activation hooks
+		register_activation_hook( __FILE__, array( $this, 'activation' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'deactivation' ) );
+		register_uninstall_hook( __FILE__, array( $this, 'deactivation' ) );
+	}
 
-    function add_css(){
-	    if (file_exists(plugin_dir_path(__FILE__).'/includes/plugin.css')) {
-		    wp_enqueue_style(
-			    'ucf-people-directory-theme-style',
-			    plugin_dir_url( __FILE__ ) . '/includes/plugin.css',
-			    false,
-			    filemtime( plugin_dir_path( __FILE__ ) . '/includes/plugin.css' ),
-			    false
-		    );
-	    }
-    }
+	function add_css() {
+		if ( file_exists( plugin_dir_path( __FILE__ ) . '/includes/plugin.css' ) ) {
+			wp_enqueue_style(
+				'ucf-people-directory-theme-style',
+				plugin_dir_url( __FILE__ ) . '/includes/plugin.css',
+				false,
+				filemtime( plugin_dir_path( __FILE__ ) . '/includes/plugin.css' ),
+				false
+			);
+		}
+	}
 
-    function add_js(){
-	    if (file_exists(plugin_dir_path(__FILE__).'/includes/plugin.js')) {
-		    wp_enqueue_script(
-			    'ucf-people-directory-theme-script',
-			    plugin_dir_url( __FILE__ ) . 'includes/plugin.js',
-			    false,
-			    filemtime( plugin_dir_path( __FILE__ ) . '/includes/plugin.js' ),
-			    false
-		    );
-	    }
-    }
-/*
-    // add rewrite rules
-    static function custom_rewrite( $wp_rewrite){
+	function add_js() {
+		if ( file_exists( plugin_dir_path( __FILE__ ) . '/includes/plugin.js' ) ) {
+			wp_enqueue_script(
+				'ucf-people-directory-theme-script',
+				plugin_dir_url( __FILE__ ) . 'includes/plugin.js',
+				false,
+				filemtime( plugin_dir_path( __FILE__ ) . '/includes/plugin.js' ),
+				false
+			);
+		}
+	}
+	/*
+		// add rewrite rules
+		static function custom_rewrite( $wp_rewrite){
 
-        add_rewrite_tag(
-            '%mycustomtag%',
-            '([^/]+)'
-        );
+			add_rewrite_tag(
+				'%mycustomtag%',
+				'([^/]+)'
+			);
 
-        /*add_rewrite_rule(
-            '^' . self::$directory_path . '/mycustomtag/([^/]+)/page/([^/]+)',
-            self::$directory_path . '?tag=$matches[1]&paged=$matches[2]',
-            'bottom'
-        );
-        add_rewrite_rule(
-            '^' . self::$directory_path . '/mycustomtag/([^/]+)',
-            'index.php?pagename=' . self::$directory_path . '&mycustomtag=$matches[1]',
-            'bottom'
-        );
-        /*add_rewrite_rule(
-            '^' . self::$directory_path . '/page/([^/]+)',
-            self::$directory_path . '?tag=$matches[1]',
-            'bottom'
-        );*//*
+			/*add_rewrite_rule(
+				'^' . self::$directory_path . '/mycustomtag/([^/]+)/page/([^/]+)',
+				self::$directory_path . '?tag=$matches[1]&paged=$matches[2]',
+				'bottom'
+			);
+			add_rewrite_rule(
+				'^' . self::$directory_path . '/mycustomtag/([^/]+)',
+				'index.php?pagename=' . self::$directory_path . '&mycustomtag=$matches[1]',
+				'bottom'
+			);
+			/*add_rewrite_rule(
+				'^' . self::$directory_path . '/page/([^/]+)',
+				self::$directory_path . '?tag=$matches[1]',
+				'bottom'
+			);*//*
 
 
     }*/
 
 
+	// run on plugin activation
+	function activation() {
+		// insert the shortcode for this plugin as a term in the taxonomy
+		ucf_people_directory_shortcode::insert_shortcode_term();
+	}
 
-    // run on plugin activation
-    function activation(){
-        // insert the shortcode for this plugin as a term in the taxonomy
-        ucf_people_directory_shortcode::insert_shortcode_term();
-    }
+	// run on plugin deactivation
+	function deactivation() {
+		ucf_people_directory_shortcode::delete_shortcode_term();
+	}
 
-    // run on plugin deactivation
-    function deactivation(){
-        ucf_people_directory_shortcode::delete_shortcode_term();
-    }
-
-    // run on plugin complete uninstall
-    function uninstall(){
-        ucf_people_directory_shortcode::delete_shortcode_term();
-    }
+	// run on plugin complete uninstall
+	function uninstall() {
+		ucf_people_directory_shortcode::delete_shortcode_term();
+	}
 
 
 }
 
 new ucf_people_directory();
 
-register_activation_hook( __FILE__, array('ucf_people_directory','activation'));
-register_deactivation_hook( __FILE__, array('ucf_people_directory','deactivation'));
-register_uninstall_hook( __FILE__, array('ucf_people_directory','deactivation'));
+register_activation_hook( __FILE__, array( 'ucf_people_directory', 'activation' ) );
+register_deactivation_hook( __FILE__, array( 'ucf_people_directory', 'deactivation' ) );
+register_uninstall_hook( __FILE__, array( 'ucf_people_directory', 'deactivation' ) );
 
