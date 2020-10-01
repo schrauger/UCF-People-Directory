aecaaacf971d48ac3cd20ecab2c224397ec9282e
Stephen Schrauger <stephen@schrauger.com>
2020-10-01 12:13:24 -0400

    keeps sub categories in the same column as the parent

--- ucf-people-directory.php
+++ ucf-people-directory.php
@@ -4,7 +4,7 @@ Plugin Name: UCF People Directory
 Description: Provides a directory for the UCF people custom post type
 
 ### Note - update version number here and in includes/shortcode.php
-Version: 2.8.1
+Version: 2.8.2
 ###
 
 Author: Stephen Schrauger
