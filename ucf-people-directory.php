0f6d167d118e5235c1793e0fd63faa4f8e5afb5f
Stephen Schrauger <stephen@schrauger.com>
2020-10-07 11:35:55 -0400

    updates wording

--- ucf-people-directory.php
+++ ucf-people-directory.php
@@ -4,7 +4,7 @@ Plugin Name: UCF People Directory
 Description: Provides a directory for the UCF people custom post type
 
 ### Note - update version number here and in includes/shortcode.php
-Version: 3.0.3
+Version: 3.0.4
 ###
 
 Author: Stephen Schrauger
